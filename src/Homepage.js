cac41bb7b059ae08ccea3f96735699318b77a810
Elizabeth friedland <elizabethfriedland@Elizabeths-MacBook.local>
2017-10-16 21:23:09 -0700

    first commit

---
+++ src/Homepage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+      <h1 className="App"> This is the Homepage</h1>
+
+      
+
+
+      </div>
+    )
+  }
+}
+
+export default Homepage;
